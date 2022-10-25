7deac16ce7a8f7acf21cf673e2afe2cc36885c4e
Analista 1 <analista1@MERCADEOVIRTUAL.local>
2022-10-25 16:48:40 -0500

    Implement seed and pagination

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,23 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import axios, { AxiosInstance } from 'axios';
+import { Model } from 'mongoose';
+
 import { PokeapiResponse } from './interfaces/pokeapi-response.interface';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
+    await this.pokemonModel.deleteMany();
+
+    const pokemonToInsert: { name: string; no: number }[] = [];
+
     const { data } = await this.axios.get<PokeapiResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
 
-      console.log({ name, no });
+      pokemonToInsert.push({
+        name,
+        no,
+      });
     });
+    await this.pokemonModel.insertMany(pokemonToInsert);
 
-    return data.results;
+    return 'Seed executed';
   }
 }
